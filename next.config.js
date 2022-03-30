2775bed9a3d9c77fe926b2ef4f5b1dee267ec817
Sean Filimon <65460667+seanfilimon@users.noreply.github.com>
2022-03-30 03:39:52 -0400

    3kr2io3h9832j40

--- next.config.js
+++ next.config.js
@@ -3,5 +3,14 @@ module.exports = {
         MONGO_URI: "mongodb+srv://root:7dApqdgaxDT7T3CG@fabwelt.ebeic.mongodb.net/nftmarket",
         API_URL: "https://localhost:3000",
     },
-    webpack5: true
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+            config.resolve.fallback = {
+                fs: false
+            }
+        }
+
+        return config;
+    }
 }
