8c64ea1d1accd17414bee74f02ac252aefcddcfa
Sean Filimon <65460667+seanfilimon@users.noreply.github.com>
2022-03-30 04:04:16 -0400

    jsiadjei

--- next.config.js
+++ next.config.js
@@ -5,10 +5,7 @@ module.exports = {
     },
     webpack: (config, { isServer }) => {
         if (!isServer) {
-            // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
-            config.resolve.fallback = {
-                fs: false
-            }
+            // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs
         }
 
         return config;
